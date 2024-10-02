ce9efad981d9983e2b2921acf5e5bcf97873e5e4
iietmoon <otmannj56@gmail.com>
2024-10-03 00:22:03 +0100

    feat: add utility function to instantiate Validator class with validation rules

--- src/types/validator.d.ts
+++ src/types/validator.d.ts
@@ -2,6 +2,9 @@ export interface ValidationRules {
   [key: string]: string;
 }
 
+export type ValidationRule = "required" | "email" | "min";
+
 export interface ValidationResult {
   [key: string]: string[];
 }
+
